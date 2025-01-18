44435671a63a76331a79906d3fd60589a9371596
yang <yang20401@naver.com>
2025-01-18 23:53:00 +0900

    [Bronze V] Title: 새싹, Time: 160 ms, Memory: 17436 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = "         ,r'\"7\n" + 	
+                "r`-_   ,'  ,/\n" + 		
+                " \\. \". L_r'\n" + 		
+                "   `~\\/\n" + 			
+                "      |\n" + 			
+                "      |";
+        
+        System.out.println(str);
+        scanner.close();
+    }
+}
