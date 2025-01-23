6e8a2418ab945c1725485edbdcd78bc7743bf04e
yang <yang20401@naver.com>
2025-01-23 17:20:37 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String string = "??!";     
+        String s = scanner.next();
+        
+        System.out.println(s + string);
+        
+        scanner.close();
+    }
+}
