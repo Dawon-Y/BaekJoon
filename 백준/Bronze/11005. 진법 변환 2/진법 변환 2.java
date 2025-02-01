187ce017032d600067e2633d84e0063f63f71f7b
yang <yang20401@naver.com>
2025-02-01 21:12:54 +0900

    [Bronze I] Title: 진법 변환 2, Time: 108 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,18 +1,30 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
-        bw.write((Integer.toString(n, b).toUpperCase()));
-        bw.flush();
-        bw.close();
+        br.close();
+
+        ArrayList<Character> list = new ArrayList<>();
+
+        while (N > 0) {
+            if (N % B < 10) {
+                list.add((char) (N % B + '0')); 
+            }
+            else{
+                list.add((char) (N % B - 10 + 'A'));
+            }
+            N /= B;
+        }
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i));
+        }
     }
 }
