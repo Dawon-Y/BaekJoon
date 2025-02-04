99d94821b2fb968050457e39c06c0678bc86dd56
yang <yang20401@naver.com>
2025-02-04 12:49:33 +0900

    [Silver I] Title: 안정적인 문자열, Time: 148 ms, Memory: 15936 KB -BaekjoonHub

---
+++ 백준/Silver/4889. 안정적인 문자열/안정적인 문자열.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String tmp;
+        int n = 1;
+        while(true) {
+            tmp = br.readLine();
+            if(tmp.charAt(0)=='-') break;
+
+            int ans =0;
+            Stack<Character> st = new Stack<>();
+
+            for (int i = 0; i < tmp.length(); i++) {
+                char c = tmp.charAt(i);
+                if(c=='{') {
+                    st.add(c);
+                }else {
+                    if(st.isEmpty()) {
+                        ans++;
+                        st.add('{');
+                    }else {
+                        st.pop();
+                    }
+                }
+            }
+            sb.append((n++) +". " + (ans+st.size()/2)+"\n" );
+        }
+        System.out.println(sb);
+    }
+}
