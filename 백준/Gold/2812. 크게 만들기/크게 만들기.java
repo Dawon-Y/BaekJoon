d72a9a087672e50b8e361360e4b500eee9434147
yang <yang20401@naver.com>
2025-02-06 02:20:14 +0900

    [Gold III] Title: 크게 만들기, Time: 368 ms, Memory: 36820 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -1,35 +1,40 @@
-import javax.swing.*;
-import java.io.*;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
-
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
+ 
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        String str = br.readLine();
-        Stack<Character> stack = new Stack<>();
-
-        int count = 0;
-        for(int i = 0; i < n; i++){
-            while(!stack.isEmpty() && count < k && stack.peek() < str.charAt(i)){
-                stack.pop();
-                count++;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+ 
+        String input = br.readLine();
+ 
+        char[] arr = input.toCharArray();
+        Deque<Character> dq = new ArrayDeque<>();
+        for (int i = 0; i < arr.length; i++) {
+            // 데크의 맨 뒤의 값이 arr[i]보다 작으면 삭제한다.
+            // 아래 조건을 만족할 때까지 반복.
+            while (K > 0 && !dq.isEmpty() && dq.getLast() < arr[i]) {
+                dq.removeLast();
+                K--;
             }
-            if(stack.size() < n - k){
-                stack.add(str.charAt(i));
-            }
+            dq.addLast(arr[i]);
         }
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i < stack.size(); i++){
-            sb.append(stack.get(i));
+ 
+        StringBuilder ans = new StringBuilder();
+        while (dq.size() > K) {
+            ans.append(dq.removeFirst());
         }
-
-        bw.write(sb + "\n");
+        bw.write(ans.toString() + "\n");
         bw.flush();
         bw.close();
         br.close();
