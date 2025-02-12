aeb8d0dbccebc3760c3910126eecaed0dbcc4cac
yang <yang20401@naver.com>
2025-02-13 00:20:30 +0900

    [Silver IV] Title: 로프, Time: 264 ms, Memory: 25540 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -4,8 +4,7 @@ import java.io.*;
 public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
 
         int[] ropes = new int[n];
         for(int i = 0; i < n; i++){
