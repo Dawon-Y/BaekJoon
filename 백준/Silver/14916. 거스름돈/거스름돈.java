ee313affb4e441d2df19a14f23c6cc738fcc5121
Dawon-Y <yang20401@naver.com>
2025-11-10 15:22:35 +0900

    [Silver V] Title: 거스름돈, Time: 112 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        while(n > 0){
+            if(n % 5 == 0){
+                count += n / 5;
+                n = 0;
+                break;
+            }
+            n -= 2;
+            count++;
+        }
+
+        if(n == 0){
+            System.out.println(count);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
